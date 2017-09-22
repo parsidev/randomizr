019d64e47d508c03efd3096349aa7eb155dc6ad9
Mohammad Reza <parsidev@users.noreply.github.com>
2017-09-22 10:47:46 +0330

    Update RandomizrServiceProvider.php

--- src/Providers/RandomizrServiceProvider.php
+++ src/Providers/RandomizrServiceProvider.php
@@ -5,65 +5,30 @@ use Parsidev\Support\Services\Randomizr;
 
 class RandomizrServiceProvider extends ServiceProvider {
 
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
     protected $defer = true;
 
-    /**
-     * Name of the class instance bound the the IoC.
-     *
-     * @var string
-     */
-    protected $instance = 'Parsidev\Support\Services\Randomizr';
-
-    /**
-     * Name of the package.
-     *
-     * @var string
-     */
-    protected $package_name = 'parsidev/randomizr';
-
-    /**
-     * Contains the transformer configuration.
-     *
-     * @var array
-     */
     protected $config;
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
     public function register()
     {
-        $this->app['config']->package( $this->package_name, __DIR__.'/../Config/');
 
-        $this->config = $this->app['config']->get('randomizr::randomizr');
-
-        $this->app->singleton( $this->instance, function()
+        $this->app->singleton("randomizr", function()
         {
             $db = $this->app['db'];
-
+            $this->config = config('randomizr');
             return new Randomizr( $this->config, $db );
         });
     }
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
     public function boot()
     {
-        // Register the package
-        $this->package( $this->package_name );
+
+        $this->publishes([
+            __DIR__ . '/../Config/randomizr.php' => config_path('randomizr.php'),
+        ]);
 
         // Register Artisan commands
-        $this->commands(array('Parsidev\Support\Commands\RandomizrPublisherCommand'));
+        $this->commands(['Parsidev\Support\Commands\RandomizrPublisherCommand']);
     }
 
     /**
@@ -73,7 +38,7 @@ class RandomizrServiceProvider extends ServiceProvider {
      */
     public function provides()
     {
-        return array($this->instance);
+        return ['randomizr'];
     }
 
 }
