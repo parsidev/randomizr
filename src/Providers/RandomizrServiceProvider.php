c6604e65a80eec501e6bec849079c476dd8af5b5
Mohammad Reza <parsidev@users.noreply.github.com>
2017-09-22 17:37:56 +0330

    Update RandomizrServiceProvider.php

--- src/Providers/RandomizrServiceProvider.php
+++ src/Providers/RandomizrServiceProvider.php
@@ -1,6 +1,4 @@
 <?php namespace Parsidev\Support\Providers;
-<<<<<<< HEAD
-    
     use Illuminate\Support\ServiceProvider;
     use Parsidev\Support\Services\Randomizr;
     
@@ -40,49 +38,3 @@
         }
         
     }
-
-=======
-
-use Illuminate\Support\ServiceProvider;
-use Parsidev\Support\Services\Randomizr;
-
-class RandomizrServiceProvider extends ServiceProvider {
-
-    protected $defer = true;
-
-    protected $config;
-
-    public function register()
-    {
-
-        $this->app->singleton("randomizr", function()
-        {
-            $db = $this->app['db'];
-            $this->config = config('randomizr');
-            return new Randomizr( $this->config, $db );
-        });
-    }
-
-    public function boot()
-    {
-
-        $this->publishes([
-            __DIR__ . '/../Config/randomizr.php' => config_path('randomizr.php'),
-        ]);
-
-        // Register Artisan commands
-        $this->commands(['Parsidev\Support\Commands\RandomizrPublisherCommand']);
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['randomizr'];
-    }
-
-}
->>>>>>> origin/master
